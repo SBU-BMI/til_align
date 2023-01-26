c2a94ef03b6f0ba5226edfd2bcf46af9062b758a
lthealy <luke.torrehealy@stonybrookmedicine.edu>
2023-01-26 17:41:57 +0000

    improved analytical ease-of-use #10

--- scripts_and_docker/renderWrapper.R
+++ scripts_and_docker/renderWrapper.R
@@ -2,31 +2,35 @@
 
 args = commandArgs(trailingOnly=TRUE)
 params = list(csvPath = args[1],
-              excludeSurv = args[2],
-              survTime = args[3],
-              survCensor = args[4],
-              outputFormat = args[5])
+              survTime = args[2],
+              survCensor = args[3],
+              outputFormat = args[4])
 print(params)
-
-## Read in dataset -- check approx num of vars to set figure height in output
 setwd("/data")
+
+## Read in dataset info -- check approx num of vars to set figure height in output (this is output from callAlign.sh)
 df =  as.data.frame(suppressMessages(readr::read_csv(file = params$csvPath)))
-params$nVars = sum(which(!colnames(df) %in% c("scaled_PP","TIL_Class",params$survCensor, params$survTime)))
+
+## ==== Drop TIL analytics that are not used ====
+# -- n_Canc_patch
+# -- n_TIL_patch
+# -- n_TIL_patch_overlap
+# -- patch_ratio
+# -- percent_pos
+droppable = c(grep("*_Canc_patch", colnames(df)), ## *_ regex used to be flexible with subtype specific patch metrics
+              grep("*_TIL_patch*", colnames(df)),
+              grep("patch_ratio", colnames(df)),
+              grep("*_percent_pos", colnames(df)))
+
+df = df[,-droppable]
+params$nVars = sum(which(!colnames(df) %in% c("slideID","scaled_PP","TIL_Class",params$survCensor, params$survTime)))
 
 ## Call render
-if(params$excludeSurv == "true"){
-   params$excludeSurv = TRUE
-   rmarkdown::render("/code/Descriptive_Statistics.rmd", params = list(csvPath = params$csvPath,
-                                                                 nVars = params$nVars,
-                                                                 survTime = params$survTime,
-                                                                 survCensor = params$survCensor),
-                     output_format = params$outputFormat,
-                     output_file = "/data/Descriptive_Statistics", knit_root_dir = "/data")
-} else {
-   params$excludeSurv = FALSE
-   rmarkdown::render("/code/Descriptive_Statistics.rmd", params = list(csvPath = params$csvPath,
-                                                                 nVars = params$nVars),
-                     output_format = params$outputFormat,
-                     output_file = "/data/Descriptive_Statistics", knit_root_dir = "/data")
-}
+rmarkdown::render("/code/Descriptive_Statistics.rmd", 
+                  params = list(csvPath = params$csvPath,
+                                nVars = params$nVars,
+                                survTime = params$survTime,
+                                survCensor = params$survCensor),
+                  output_format = params$outputFormat,
+                  output_file = "/data/Descriptive_Statistics", knit_root_dir = "/data")
 
